ae8bbe953d2b19746531110e9c4390731587470e
unknown <596230@stud.hvl.no>
2023-04-15 22:26:09 +0200

    done

--- src/main/java/no/hvl/dat110/middleware/MutualExclusion.java
+++ src/main/java/no/hvl/dat110/middleware/MutualExclusion.java
@@ -21,195 +21,223 @@ import no.hvl.dat110.util.Util;
  *
  */
 public class MutualExclusion {
-		
+
 	private static final Logger logger = LogManager.getLogger(MutualExclusion.class);
 	/** lock variables */
-	private boolean CS_BUSY = false;						// indicate to be in critical section (accessing a shared resource) 
-	private boolean WANTS_TO_ENTER_CS = false;				// indicate to want to enter CS
-	private List<Message> queueack; 						// queue for acknowledged messages
-	private List<Message> mutexqueue;						// queue for storing process that are denied permission. We really don't need this for quorum-protocol
-	
-	private LamportClock clock;								// lamport clock
+	private boolean CS_BUSY = false; // indicate to be in critical section (accessing a shared resource)
+	private boolean WANTS_TO_ENTER_CS = false; // indicate to want to enter CS
+	private List<Message> queueack; // queue for acknowledged messages
+	private List<Message> mutexqueue; // queue for storing process that are denied permission. We really don't need
+										// this for quorum-protocol
+
+	private LamportClock clock; // lamport clock
 	private Node node;
-	
+
 	public MutualExclusion(Node node) throws RemoteException {
 		this.node = node;
-		
+
 		clock = new LamportClock();
 		queueack = new ArrayList<Message>();
 		mutexqueue = new ArrayList<Message>();
 	}
-	
+
 	public synchronized void acquireLock() {
 		CS_BUSY = true;
 	}
-	
+
 	public void releaseLocks() {
 		WANTS_TO_ENTER_CS = false;
 		CS_BUSY = false;
 	}
 
 	public boolean doMutexRequest(Message message, byte[] updates) throws RemoteException {
-		
+
 		logger.info(node.nodename + " wants to access CS");
 		// clear the queueack before requesting for votes
-		
+		queueack.clear();
 		// clear the mutexqueue
-		
+		mutexqueue.clear();
 		// increment clock
-		
+		clock.increment();
 		// adjust the clock on the message, by calling the setClock on the message
-				
+		message.setClock(clock.getClock());
 		// wants to access resource - set the appropriate lock variable
-	
-		
-		// start MutualExclusion algorithm
-		
-		// first, call removeDuplicatePeersBeforeVoting. A peer can hold/contain 2 replicas of a file. This peer will appear twice
+		WANTS_TO_ENTER_CS = true;
 
+		// start MutualExclusion algorithm
+		// first, call removeDuplicatePeersBeforeVoting. A peer can hold/contain 2
+		// replicas of a file. This peer will appear twice
+		List<Message> active = removeDuplicatePeersBeforeVoting();
 		// multicast the message to activenodes (hint: use multicastMessage)
-		
+		multicastMessage(message, active);
 		// check that all replicas have replied (permission)
-		
+		boolean permission = areAllMessagesReturned(Util.numReplicas);
 		// if yes, acquireLock
-		
-		// node.broadcastUpdatetoPeers
-		
-		// clear the mutexqueue
-		
+		if (permission) {
+			acquireLock();
+			// node.broadcastUpdatetoPeers
+			node.broadcastUpdatetoPeers(updates);
+			// clear the mutexqueue
+			mutexqueue.clear();
+		}
+
 		// return permission
-		
-		return false;
+		return permission;
 	}
-	
+
 	// multicast message to other processes including self
 	private void multicastMessage(Message message, List<Message> activenodes) throws RemoteException {
-		
-		logger.info("Number of peers to vote = "+activenodes.size());
-		
+
+		logger.info("Number of peers to vote = " + activenodes.size());
+
 		// iterate over the activenodes
-		
-		// obtain a stub for each node from the registry
-		
-		// call onMutexRequestReceived()
-		
+		for (Message m : activenodes) {
+			// obtain a stub for each node from the registry
+			Util.getProcessStub(m.getNodeName(), m.getPort()).onMutexAcknowledgementReceived(m);
+		}
+
 	}
-	
+
 	public void onMutexRequestReceived(Message message) throws RemoteException {
-		
+
 		// increment the local clock
-		
-		// if message is from self, acknowledge, and call onMutexAcknowledgementReceived()
-			
-		int caseid = -1;
-		
-		/* write if statement to transition to the correct caseid */
-		
-		// caseid=0: Receiver is not accessing shared resource and does not want to (send OK to sender)
-		
-		// caseid=1: Receiver already has access to the resource (dont reply but queue the request)
-		
-		// caseid=2: Receiver wants to access resource but is yet to - compare own message clock to received message's clock
-		
-		// check for decision
-		doDecisionAlgorithm(message, mutexqueue, caseid);
+		clock.increment();
+
+		if (message.getNodeID().compareTo(node.getNodeID()) == 0) {
+			message.setAcknowledged(true);
+			onMutexAcknowledgementReceived(message);
+		} else {
+			int caseid = -1;
+			if (!CS_BUSY && !WANTS_TO_ENTER_CS)
+				caseid = 0;
+			else if (CS_BUSY)
+				caseid = 1;
+			else if (WANTS_TO_ENTER_CS)
+				caseid = 2;
+			// check for decision
+			doDecisionAlgorithm(message, mutexqueue, caseid);
+		}
+
 	}
-	
+
 	public void doDecisionAlgorithm(Message message, List<Message> queue, int condition) throws RemoteException {
-		
+
 		String procName = message.getNodeName();
 		int port = message.getPort();
-		
-		switch(condition) {
-		
-			/** case 1: Receiver is not accessing shared resource and does not want to (send OK to sender) */
-			case 0: {
-				
-				// get a stub for the sender from the registry
-				
-				// acknowledge message
-				
-				// send acknowledgement back by calling onMutexAcknowledgementReceived()
-				
-				break;
-			}
-		
-			/** case 2: Receiver already has access to the resource (dont reply but queue the request) */
-			case 1: {
-				
-				// queue this message
-				break;
-			}
-			
+
+		switch (condition) {
+
 			/**
-			 *  case 3: Receiver wants to access resource but is yet to (compare own message clock to received message's clock
-			 *  the message with lower timestamp wins) - send OK if received is lower. Queue message if received is higher
+			 * case 1: Receiver is not accessing shared resource and does not want to (send
+			 * OK to sender)
+			 */
+			case 0: {
+				// acknowledge message
+				message.setAcknowledged(true);
+				// send acknowledgement back by calling onMutexAcknowledgementReceived()
+				Util.getProcessStub(procName, port).onMutexAcknowledgementReceived(message);
+				break;
+			}
+
+			/**
+			 * case 2: Receiver already has access to the resource (dont reply but queue the
+			 * request)
+			 */
+			case 1: {
+
+				// queue this message
+				queue.add(message);
+				break;
+			}
+
+			/**
+			 * case 3: Receiver wants to access resource but is yet to (compare own message
+			 * clock to received message's clock
+			 * the message with lower timestamp wins) - send OK if received is lower. Queue
+			 * message if received is higher
 			 */
 			case 2: {
-				
-				// check the clock of the sending process (note that the correct clock is in the message)
-				
-				// own clock for the multicast message (note that the correct clock is in the message)
-				
+				// check the clock of the sending process (note that the correct clock is in the
+				// message)
+				int senderClock = message.getClock();
+				int l = node.getMessage().getClock();
+				// own clock for the multicast message (note that the correct clock is in the
+				// message)
 				// compare clocks, the lowest wins
-				
 				// if clocks are the same, compare nodeIDs, the lowest wins
-				
-				// if sender wins, acknowledge the message, obtain a stub and call onMutexAcknowledgementReceived()
-				
-				// if sender looses, queue it
-
+				if (senderClock == l) {
+					// if clocks are the same, compare nodeIDs, the lowest wins
+					if (node.getNodeID().compareTo(message.getNodeID()) == -1) {
+						queue.add(message);
+					} else {
+						message.setAcknowledged(true);
+						Util.getProcessStub(procName, port).onMutexAcknowledgementReceived(message);
+					}
+				} else if (senderClock < l) {
+					// if sender wins, acknowledge the message, obtain a stub and call
+					// onMutexAcknowledgementReceived()
+					message.setAcknowledged(true);
+					Util.getProcessStub(procName, port).onMutexAcknowledgementReceived(message);
+				} else {
+					// if sender looses, queue it
+					queue.add(message);
+				}
 				break;
 			}
-			
-			default: break;
+			default:
+				break;
 		}
-		
+
 	}
-	
+
 	public void onMutexAcknowledgementReceived(Message message) throws RemoteException {
-		
+
 		// add message to queueack
-		
+		queueack.add(message);
 	}
-	
+
 	// multicast release locks message to other processes including self
 	public void multicastReleaseLocks(Set<Message> activenodes) {
-		logger.info("Releasing locks from = "+activenodes.size());
-		
+		logger.info("Releasing locks from = " + activenodes.size());
 		// iterate over the activenodes
-		
-		// obtain a stub for each node from the registry
-		
-		// call releaseLocks()	
+		for (Message m : activenodes) {
+			// call releaseLocks()
+			NodeInterface stub = Util.getProcessStub(m.getNodeName(), m.getPort());
+			try {
+				stub.releaseLocks();
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-	
+
 	private boolean areAllMessagesReturned(int numvoters) throws RemoteException {
-		logger.info(node.getNodeName()+": size of queueack = "+queueack.size());
-		
+		logger.info(node.getNodeName() + ": size of queueack = " + queueack.size());
+
 		// check if the size of the queueack is same as the numvoters
-		
-		// clear the queueack
-		
-		// return true if yes and false if no
-		
+		if (queueack.size() == numvoters) {
+			// clear the queueack
+			queueack.clear();
+			return true;
+		}
+		queueack.clear();
 		return false;
 	}
-	
+
 	private List<Message> removeDuplicatePeersBeforeVoting() {
-		
+
 		List<Message> uniquepeer = new ArrayList<Message>();
-		for(Message p : node.activenodesforfile) {
+		for (Message p : node.activenodesforfile) {
 			boolean found = false;
-			for(Message p1 : uniquepeer) {
-				if(p.getNodeName().equals(p1.getNodeName())) {
+			for (Message p1 : uniquepeer) {
+				if (p.getNodeName().equals(p1.getNodeName())) {
 					found = true;
 					break;
 				}
 			}
-			if(!found)
+			if (!found)
 				uniquepeer.add(p);
-		}		
+		}
 		return uniquepeer;
 	}
 }
--- src/main/java/no/hvl/dat110/util/Hash.java
+++ src/main/java/no/hvl/dat110/util/Hash.java
@@ -6,59 +6,58 @@ package no.hvl.dat110.util;
  *
  */
 
-import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class Hash { 
-	
-	
-	public static BigInteger hashOf(String entity) {	
-		
+public class Hash {
+
+	public static BigInteger hashOf(String entity) {
+
 		BigInteger hashint = null;
-		
-		// Task: Hash a given string using MD5 and return the result as a BigInteger.
-		
-		// we use MD5 with 128 bits digest
-		
-		// compute the hash of the input 'entity'
-		
-		// convert the hash into hex format
-		
-		// convert the hex into BigInteger
-		
-		// return the BigInteger
-		
+		try {
+			// use the MD5 hash algo
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			// calculate the hash value of the input entity as an array of bytes
+			byte[] hash = md.digest(entity.getBytes());
+			// convert the byte array into a hexadecimal string representation
+			String hex = toHex(hash);
+			hashint = new BigInteger(hex, 16);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
+		// return BigInt hash value
 		return hashint;
 	}
-	
+
 	public static BigInteger addressSize() {
-		
-		// Task: compute the address size of MD5
-		
 		// compute the number of bits = bitSize()
-		
+		int bits = bitSize();
 		// compute the address size = 2 ^ number of bits
-		
+		BigInteger addressSize = new BigInteger("2").pow(bits);
 		// return the address size
-		
-		return null;
+		return addressSize;
 	}
-	
+
 	public static int bitSize() {
-		
+
 		int digestlen = 0;
-		
-		// find the digest length
-		
-		return digestlen*8;
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			// get len of the md5 hash digest in bytes
+			digestlen = md.getDigestLength();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		// return the bit size
+		return digestlen * 8;
 	}
-	
+
 	public static String toHex(byte[] digest) {
 		StringBuilder strbuilder = new StringBuilder();
-		for(byte b : digest) {
-			strbuilder.append(String.format("%02x", b&0xff));
+		for (byte b : digest) {
+			strbuilder.append(String.format("%02x", b & 0xff));
 		}
 		return strbuilder.toString();
 	}
